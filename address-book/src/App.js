7f07af478adb498a3272cf7307fff4a12c044cee
unknown <xander.mcgowan@gmail.com>
2023-08-06 14:45:30 -0500

    final verson

--- address-book/src/App.js
+++ address-book/src/App.js
@@ -1,37 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import LogicalNot from './LogicalNot';
- 
-import "./App.css"
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
   const [users, setUsers] = useState();
 
   useEffect(() => {
     const getUsers = async () => {
-      const response = await fetch(
-        'https://randomuser.me/api?results=25'
-      );
+      const response = await fetch("https://randomuser.me/api?results=25");
       const data = await response.json();
       setUsers(data);
     };
     getUsers();
   }, []);
 
-    
-
-
   console.log(users);
 
   return (
-   <div>
+    <div>
       {users ? (
         users.results.map((user) => (
           <div class="main" key={user.id.value}>
-             <img src={user.picture.thumbnail} />
-            <h2>User: {user.name.first} {user.name.last}</h2>
-            <div className="App mt-5">
-      <LogicalNot />
-    </div>
+            <img src={user.picture.thumbnail} />
+            <h2>
+              User: {user.name.first} {user.name.last}
+            </h2>
+            <ul>
+              <li>{user.location.street.number} {user.location.street.name} </li>
+              <li>{user.location.city}, {user.location.country}</li>
+            </ul>
           </div>
         ))
       ) : (
